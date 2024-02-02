2077e60d8e74e887492798ab3dc2f5ccbdb981fe
Amirhossein Najafi <njamir16@gmail.com>
2024-02-02 11:23:11 +0330

    Add files via upload
    added files

---
+++ data/Review.sql
@@ -0,0 +1,15 @@
+insert into Review (result_exam, crew_id, aircraft_id) values (7, 23, 11);
+insert into Review (result_exam, crew_id, aircraft_id) values (13, 9, 20);
+insert into Review (result_exam, crew_id, aircraft_id) values (13, 18, 8);
+insert into Review (result_exam, crew_id, aircraft_id) values (4, 35, 18);
+insert into Review (result_exam, crew_id, aircraft_id) values (7, 23, 20);
+insert into Review (result_exam, crew_id, aircraft_id) values (8, 14, 9);
+insert into Review (result_exam, crew_id, aircraft_id) values (7, 37, 8);
+insert into Review (result_exam, crew_id, aircraft_id) values (8, 28, 3);
+insert into Review (result_exam, crew_id, aircraft_id) values (19, 1, 14);
+insert into Review (result_exam, crew_id, aircraft_id) values (4, 13, 8);
+insert into Review (result_exam, crew_id, aircraft_id) values (8, 10, 7);
+insert into Review (result_exam, crew_id, aircraft_id) values (18, 29, 19);
+insert into Review (result_exam, crew_id, aircraft_id) values (20, 24, 4);
+insert into Review (result_exam, crew_id, aircraft_id) values (19, 26, 11);
+insert into Review (result_exam, crew_id, aircraft_id) values (2, 17, 16);
